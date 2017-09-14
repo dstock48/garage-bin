d25bf6c5798ae7990984c905b0d77f7352f4ee39
Dave W <dviper@gmail.com>
2017-09-14 09:43:53 -0600

    Setup knexfile for dev, test, and prod

---
+++ knexfile.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/garage_bin',
+    migrations: { directory: './db/migrations' },
+    useNullAsDefault: true
+  },
+
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/garage_bin_test',
+    useNullAsDefault: true,
+    migrations: { directory: './db/migrations' },
+    seeds: { directory: './db/seeds/test' }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: { directory: './db/migrations' },
+    useNullAsDefault: true
+  }
+
+};
