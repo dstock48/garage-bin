ccca09db845251f85311cbd54c8b4d781386bba2
Dave W <dviper@gmail.com>
2017-09-14 11:57:48 -0600

    Remove "id" key from seeded data objects

--- db/seeds/dev/garage-items.js
+++ db/seeds/dev/garage-items.js
@@ -5,19 +5,16 @@ exports.seed = (knex, Promise) => {
       return Promise.all([
         knex('garage').insert([
           {
-            id: 1,
             item_name: 'shovel',
             reason: 'digging holes in the backyard',
             cleanliness: 'dusty'
           },
           {
-            id: 2,
             item_name: 'garbage bags',
             reason: 'bagging items to bury in holes',
             cleanliness: 'sparkling'
           },
           {
-            id: 3,
             item_name: 'hacksaw',
             reason: 'cutting things apart to place in bags and bury in backyard',
             cleanliness: 'rancid'
