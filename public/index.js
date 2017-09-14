fee923c275e3104446cfd8a4484633d9cf7656ff
Dave W <dviper@gmail.com>
2017-09-14 13:55:12 -0600

    Add ability to add new items to garage from page

--- public/index.js
+++ public/index.js
@@ -1,18 +1,52 @@
+const inputForm = document.querySelector('#new-item-inputs');
+
 getGarageItems();
+
+inputForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const name = $('#item-name-input');
+  const reason = $('#item-reason-input');
+  const cleanliness = $('#item-cleanliness-input');
+
+  fetch('/api/v1/item', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      item_name: name.val(),
+      reason: reason.val(),
+      cleanliness: cleanliness.val()
+    })
+  })
+    .then(data => data.json())
+    .then((item) => {
+      appendItem(item);
+
+      name.val('');
+      reason.val('');
+      cleanliness.val('select');
+    });
+});
+
+
+// FUNCTIONS //////////////////////////////////
+///////////////////////////////////////////////
+
+function appendItem(item) {
+  $('#garage-items').append(`
+    <div class="item-card">
+      <h2 class="item-name">${item.item_name}</h2>
+      <p class="item-reason"><span class="label">Linger Reason:</span> ${item.reason}</p>
+      <p class="item-cleanliness"><span class="label">Cleanliness:</span> ${item.cleanliness}</p>
+    </div>
+  `);
+}
 
 function getGarageItems() {
   fetch('/api/v1/item')
     .then(data => data.json())
     .then(items => {
       items.forEach(item => {
-        $('#garage-items').append(`
-          <div class="item-card">
-            <h2 class="item-name">${item.item_name}</h2>
-            <p class="item-reason"><span class="label">Linger Reason:</span> ${item.reason}</p>
-
-            <p class="item-cleanliness"><span class="label">Cleanliness:</span> ${item.cleanliness}</p>
-          </div>
-        `);
+        appendItem(item);
       });
     });
 }
